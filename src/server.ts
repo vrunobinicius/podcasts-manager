1fb1d43eb44321d219fc8bf0f5c9b6fdfce8a9dc
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2025-09-16 22:16:18 -0300

    chore: set up the initial server boilerplate
    - Adds the basic structure for an HTTP server to listen for requests.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import * as http from "http";
+
+const server = http.createServer(
+    (request: http.IncomingMessage,
+        response: http.ServerResponse) => {
+
+    }
+);
+
+const port = process.env.PORT;
+
+server.listen(port, () => { console.log(`SERVIDOR INICIADO NA PORTA ${port}`); });
